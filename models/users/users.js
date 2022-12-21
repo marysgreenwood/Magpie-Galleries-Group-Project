82e5d60fbf02f6a7ee6a4092bbd45b307f2b4b0f
GhostoftheMill <kevinmichaelwhite@gmail.com>
2022-12-20 19:49:31 -0500

    Created user model

---
+++ models/users/users.js
@@ -0,0 +1,31 @@
+const { Model, DateTypes } = require('sequalize');
+
+const sequelize = require(/*insert location of env js path*/);
+
+class Users extends Model {}
+
+Users.init(
+//columns Username, Passwordhash, id(primarykey)
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+  },
+        username: {
+            type: DataTypes.STRING
+  },
+        passwordhash: {
+            type: DataTypes.//not sure yet
+},
+{
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'book'
+}
+);
+
+module.exports = Users;
+
