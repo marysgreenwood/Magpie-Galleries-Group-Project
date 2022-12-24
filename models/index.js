f6cf7a48e6edbaad03a2bb57bd40fef1af0c7a39
Adem Sahil <adem.sahil274@gmail.com>
2022-12-23 20:12:48 -0500

    models-index NEW

--- models/index.js
+++ models/index.js
@@ -1,6 +1,14 @@
-const User = require ('./users/user');
+const User = require ('./users/users');
 const Art = require ('./art');
 
+ User.hasMany(Art, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Art.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
 module.exports = { Art, User };
 
