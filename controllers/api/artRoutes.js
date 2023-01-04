62269c1e154e7c15df48b771d8bc04f97f53dbee
marysgreenwood <marysgreenwood@gmail.com>
2023-01-04 11:50:45 -0500

    added post route for uploading user art

--- controllers/api/artRoutes.js
+++ controllers/api/artRoutes.js
@@ -1,17 +1,20 @@
 const router = require('express').Router();
 const multer = require('multer');
 const { Art } = require('../../models');
-const upload = multer({ dest: "../assets/user-art"});
+const upload = require("../../utils/upload")
 
 //post route to add new art
 
-/*app.post("/upload_files", upload.array("files"), uploadFiles);
-
-function uploadFiles(req, res) {
-    console.log(req.body);
-    console.log(req.files);
-    res.json({ message: "Successfully uploaded files" });
-}*/
+router.post ('/upload', upload.single("file"), async (req, res) => {
+    try{
+        const newArt= req.body;
+        newArt.image= req.file;
+        const artUpload = await Art.create (newArt);
+        res.status(200).json(artUpload);
+    } catch(err){
+        res.status(400).json(err);
+    }
+})
 
 //put route to update title and description fields
 //delete route to remove work
