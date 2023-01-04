e1d090056ebf08f5cd86d54afd6d28e025ec7148
marysgreenwood <marysgreenwood@gmail.com>
2023-01-04 12:29:30 -0500

    added routes to update & delete user art

--- controllers/api/artRoutes.js
+++ controllers/api/artRoutes.js
@@ -14,7 +14,38 @@ router.post ('/upload', upload.single("file"), async (req, res) => {
     } catch(err){
         res.status(400).json(err);
     }
-})
+});
 
 //put route to update title and description fields
+router.put('/:id', (req, res) => {
+  Art.update(
+    {
+      title: req.body.title,
+      description: req.body.description,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedArt) => {
+      res.json(updatedArt);
+    })
+    .catch((err) => res.json(err));
+});
+
 //delete route to remove work
+router.delete (':id', async (req, res) => {
+    try {
+        const deletedArt= await Art.destroy ({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(deletedArt)
+
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
