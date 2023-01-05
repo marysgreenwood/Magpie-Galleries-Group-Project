675e4ec3715a54985b7671f70c004a4978f05070
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-01-05 18:35:24 -0500

    Changed file extensions on handlebar files

--- public/js/hblanding.js
+++ public/js/hblanding.js
@@ -1,8 +1,8 @@
+//import { Users } from './models/users/users.js';
+
 const source = document.getElementById('landing').innerHTML;
 const template = Handlebars.compile(source);
-const context = {
-  
-}
+const context = {Users}
 const compiledHtml = template(context);
 const fill = document.getElementById('');
 fill.innerHTML = compiledHtml;
