4c9c9b1b6a9e0535ad38b4101e8847f56686029a
marysgreenwood <marysgreenwood@gmail.com>
2023-01-05 14:20:33 -0500

    Integrated Colin's middleware & routes with existing file structure

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +16,28 @@ const PORT = process.env.PORT || 3001;
 //WHICH DO WE USE?
 //const hbs = exphbs.create({ helpers });
 //var hbs = exphbs.create({ /* config */ })
+
+// initialize cookie-parser to allow us access the cookies stored in the browser. 
+app.use(cookieParser());
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+  key: 'user_sid',
+  secret: 'somerandonstuffs',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+      expires: 600000
+  }
+}));
+
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+      res.clearCookie('user_sid');        
+  }
+  next();
+});
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
@@ -24,5 +50,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
