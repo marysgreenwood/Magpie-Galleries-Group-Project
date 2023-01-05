19a2f4a1e504b81b12ea4fae2df01cc4fb91b5f5
marysgreenwood <marysgreenwood@gmail.com>
2023-01-05 12:52:14 -0500

    integrated Colin's code with server.js file

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const path = require('path');
 const express = require('express');
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const multer = require('multer');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Create the Handlebars.js engine object with custom helper functions
-const hbs = exphbs.create({ helpers });
+//WHICH DO WE USE?
+//const hbs = exphbs.create({ helpers });
+//var hbs = exphbs.create({ /* config */ })
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
