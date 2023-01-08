d6b4f9b130f84633837a985b41c9d77e7505ddb0
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-01-08 08:35:32 -0500

    Added generic query selector variables for various elements

---
+++ public/js/event.js
@@ -0,0 +1,28 @@
+//querySelector for different elements. Need to go back and insert element id where appropriate, and maybe move to function scope to limit globally declared variables
+const loginSelect = document.querySelector('#insertID');
+const addSelect = document.querySelector('#insertID');
+const searchGo = document.querySelector('#insertID');
+const searchInput = document.querySelector('#insertID');
+const saveSelect = document.querySelector('#insertID');
+const logOut = document.querySelector('#insertID');
+const editPic = document.querySelector('#insertID');
+const editName = document.querySelector('#insertID');
+const editPronouns = document.querySelector('#insertID');
+const editBio = document.querySelector('#insertID');
+
+//generic click EventLister
+/*element selector variable*/.addEventListener('click', /*function to perform*/);
+
+// when user clicks login, they should be prompted asking for username and password
+
+//when user clicks add button they should be taking somewhere to upload art
+
+//when user clicks searchbutton database should be searched 
+
+// example function 
+/*function writePassword() {
+  
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+} */
