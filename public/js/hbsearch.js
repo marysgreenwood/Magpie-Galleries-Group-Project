d6e0e852fcdb68a4b271d0cbce43488475d9c75e
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-01-09 14:06:32 -0500

    Added unsed index handlebars file

--- public/js/hbsearch.js
+++ public/js/hbsearch.js
@@ -1,7 +1,7 @@
 const source = document.getElementById('search').innerHTML;
 const template = Handlebars.compile(source);
 const context = {
-  
+  //FIGURE OUT HOW SEARCH FUNCTION NEEDS TO WORK, WHAT ARE WE SEARCHING
 }
 const compiledHtml = template(context);
 const fill = document.getElementById('');
