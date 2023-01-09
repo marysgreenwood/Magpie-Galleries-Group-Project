bc4dc06ce7ac9f46f725eaf485d085e883752f2a
GhostoftheMill <kevinmichaelwhite@gmail.com>
2023-01-09 15:44:44 -0500

    Work on handlebars functionality

--- public/js/hblanding.js
+++ public/js/hblanding.js
@@ -1,8 +1,9 @@
-//import { Users } from './models/users/users.js';
+import { Users } from './models/users/users.js';
+import { Art } from './models/art.js';
 
 const source = document.getElementById('landing').innerHTML;
 const template = Handlebars.compile(source);
-const context = {Users}
+const context = {Users, Art}
 const compiledHtml = template(context);
 const fill = document.getElementById('');
 fill.innerHTML = compiledHtml;
