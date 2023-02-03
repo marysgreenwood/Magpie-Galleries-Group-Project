7c8563600ca640a97cf7501786215a087272e8f1
marysgreenwood <marysgreenwood@gmail.com>
2023-02-02 19:48:28 -0500

    adding search results template

--- public/js/hbsearch.js
+++ public/js/hbsearch.js
@@ -1,13 +1,35 @@
-const artSearch = async (event) => {
+var searchResults = {};
+var searchResultsTemplate = document.querySelector(
+  "#searchResultsDisplay"
+).innerHTML;
+const resultsDisplay = Handlebars.compile(searchResultsTemplate);
+
+var artSearch = async (event) => {
   event.preventDefault();
   const source = document.getElementById("userSearchInput").value.trim();
-  const response = await fetch(`/api/art/search/${source}`, { method: "GET" });
-  if (!response.ok) {
-    alert("invalid search");
+  console.log("userinput", source);
+  const response = await fetch(`/api/art/search/${source}`, {
+    method: "GET",
+  });
+  if (response.ok) {
+    console.log("response", response);
+    searchResults = await response.json();
+
+    console.log("response body", searchResults);
+    resultsDisplay(searchResults);
+    //window.location.replace("/");
   }
 };
 
+// populateSearch = (event) => {
+//   event.preventDefault();
+//   artSearch().then(() => {
+//     window.location.reload();
+//   });
+// };
+
 document.querySelector("#searchBtn").addEventListener("click", artSearch);
+
 // const template = Handlebars.compile(source);
 // const context = {
 //   //FIGURE OUT HOW SEARCH FUNCTION NEEDS TO WORK, WHAT ARE WE SEARCHING
